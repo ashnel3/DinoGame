6dc7f92a1537a611608868354111ec9cefe207f5
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2022-02-17 17:00:17 -0800

    fix: scene manager animations

--- src/globals/scene_manager/SceneManager.gd
+++ src/globals/scene_manager/SceneManager.gd
@@ -3,10 +3,8 @@ extends Node2D
 onready var animation_player = $AnimationPlayer
 
 # TODO: implement transition delay
-# FIXME: scene changes before animation is finished
 func set_scene(path: String, transition: String = "fade", speed: float = 1.0, delay: float = 0):
-    animation_player.playback_speed = speed
-    animation_player.play(transition)
+    animation_player.play(transition, -1, speed)
     yield(animation_player, "animation_finished")
     assert(get_tree().change_scene(path) == OK)
-    animation_player.play_backwards(transition)
+    animation_player.play(transition, -1, -speed, true)
